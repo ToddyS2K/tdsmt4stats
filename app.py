c695e268444c0a57e27788349298f71b76843580
Marco T. <38466010+ToddyS2K@users.noreply.github.com>
2025-05-21 11:44:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ if uploaded_file is not None:
     min_date = df['Open Date'].min()
     max_date = df['Close Date'].max()
 
-    start_date = st.date_input("📅 Data inizio filtro", min_value=min_date.date(), max_value=max_date.date(), value=min_date.date(), )
-    end_date = st.date_input("📅 Data fine filtro", min_value=min_date.date(), max_value=max_date.date(), value=max_date.date())
+    start_date = st.date_input("📅 Data inizio filtro", min_value=min_date.date(), max_value=max_date.date(), value=min_date.date(), key="start_date")
+    end_date = st.date_input("📅 Data fine filtro", min_value=min_date.date(), max_value=max_date.date(), value=max_date.date(), key="end_date")
 
     df = df[(df['Open Date'].dt.date >= start_date) & (df['Open Date'].dt.date <= end_date)]
 
